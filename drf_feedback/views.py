26f0740621a76bd94cef5b85b812fb8d7e7f4fe6
Himanshu Shankar <me@himanshus.com>
2018-07-06 12:20:22 +0530

    Fixed `url` in `setup.py` Fixed `views`

--- drf_feedback/views.py
+++ drf_feedback/views.py
@@ -15,7 +15,7 @@ class AddFeedbackView(CreateAPIView):
 
     def perform_create(self, serializer):
         from drfaddons.add_ons import get_client_ip
-        if self.request.user.is_authenticated():
+        if self.request.user.is_authenticated:
             serializer.save(created_by=self.request.user, ip=get_client_ip(self.request))
         else:
             serializer.save(created_by=None, ip=get_client_ip(self.request))
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/civilmachines/drf-drf_feedback',
+    url='https://github.com/civilmachines/drf-feedback',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
